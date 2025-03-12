5490a713da607abb5e66832fd846a38770bb9aec
parkshinjo <sinjo1207@gmail.com>
2025-03-12 23:00:55 +0900

    auto commit

---
+++ 2025/Mycpp/CH3_2/src/CH3_2.cpp
@@ -0,0 +1,488 @@
+// 1) 메뉴 [File]->[New]->[C/C++ Project] 한 후 새로운 프로젝트 CH3_1을 만들어라.
+// 2) 메뉴 [File]->[New]->[Source Folder] 한 후 폴더 이름으로 src를 지정하여 생성하라.
+// 3) 메뉴 [File]->[New]->[Source File]   한 후 폴더 이름으로는 위에서 지정한 CH3_1/src로 설정하고
+//    파일 이름으로 ch3_1.cpp를 지정하여 소스파일을 만든 후 아래 코드들을([프로그램 실행] 앞까지) 삽입하라.
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// 주의: 아래 소스 코드들을 복사할 때 /* */와 같은 주석 문장을 함께 복사할 경우 주석 뒤쪽에 있는
+//      소스코드의 인덴트(공백을 안으로 밀어 넣어 주는 것)가 4칸이 아니라 2~3칸으로 인덴트가 줄어드는
+//      문제가 발생할 수 있다. 이 경우 /* */와 같은 주석을 제외하고 소스코드만 먼저 복사한 후
+//      나중에 별도로 /* */만 복사하면 괜찮아질 것이다.
+//-----------------------------------------------------------------------------
+
+/*
+ * CH3_1: ch3_1.cpp
+ *
+ *  Created on: 2025. 3. 11.
+ *      Author: Shin Jo Park
+ *
+ *  + CurrentUser 클래스 추가
+ *     Person 클래스의 멤버 함수 호출 및 구현 여부 파악
+ *  + MultiManager 클래스 추가
+ */
+
+//-----------------------------------------------------------------------------
+// 주의: 아래 헤드 파일을 include하는 < > 표시가 oj 시스템에서 HTML tag로 잘못 인식되기 때문에
+//      아래와 같이 스페이스로 분리하였다. 그렇지만 실제 프로그램 소스에서는 붙여서 표기하기 바란다.
+//-----------------------------------------------------------------------------
+#include <iostream>
+#include <cstring>
+
+using namespace std;  // 헤드 파일은 반드시 이 문장 앞쪽에 include해야 한다.
+
+/******************************************************************************
+ * 아래 상수 정의는 필요에 따라 변경하여 사용하라.
+ ******************************************************************************/
+#define AUTOMATIC_ERROR_CHECK true // true: 자동 오류 체크, true: 키보드에서 직접 입력하여 프로그램 실행
+
+/******************************************************************************
+ * Person structure and its manipulation functions
+ ******************************************************************************/
+class Person
+{
+    char   name[20];        // 이름
+    int    id;              // Identifier
+    double weight;          // 체중
+    bool   married;         // 결혼여부
+    char   address[40];     // 주소
+
+protected:
+    void inputMembers(istream* in);
+    void printMembers(ostream* out);
+
+public:
+    Person();
+    Person(const char *name);
+    Person(const char *name, int id, double weight, bool married, const char *address);
+    ~Person();
+
+    void set(const char *pname, int pid, double pweight, bool pmarried, const char *paddress);
+    void setName(const char* pname)       { strcpy(name, pname); }
+    void setId(int pid)                   { id = pid; }
+    void setWeight(double pweight)        { weight = pweight; }
+    void setMarried(bool pmarried)        { married = pmarried; }
+    void setAddress(const char* paddress) { strcpy(address, paddress);}
+
+    const char* getName()    { return name; }
+    int         getId()      { return id; }
+    double      getWeight()  {return weight; }  // 구현 시
+    bool        getMarried() {return married; }  // 리턴 값들을
+    const char* getAddress() {return address; } // 수정하시오.
+
+    void input(istream* pin)  { inputMembers(pin); } // ch3_2에서 추가
+    void print(ostream* pout) { printMembers(pout); }
+    void println()            { print(&cout); cout << endl; }
+    void whatAreYouDoing();                          // ch3_2에서 추가
+    bool isSame(const char* pname, int pid);         // ch3_2에서 추가
+};
+
+Person::Person(): name{}, id{}, weight{}, married{}, address{} {
+    // 위 함수 서두(:와 함수 본체 사이)에서 각 멤버를 초기화하는데 이는 함수 진입하기 전에
+    // 각 멤버의 값을 초기화하는 것이다. {}는 각 데이타 타입별로 디폴트 값으로 초기화하라는 의미임.
+    // 즉, name[]={'\0'}="", id=0, weight=0.0, married=false, address[]={'\0'}=""
+    cout << "Person::Person():"; println();
+}
+
+Person::Person(const char *name){
+    setName(name);
+    id = 0;
+    weight = 0;
+    married = false;
+    strcpy(address, "\0");
+    cout << "Person::Person(\"" << name << "\"):"; println();
+}
+
+Person::Person(const char *name, int id, double weight, bool married,
+               const char *address) :
+        id{id}, weight{weight}, married{married} {
+    setName(name);
+    this->id = id;
+    this->weight = weight;
+    this->married = married;
+    setAddress(address);
+    cout << "Person::Person(...):"; println();
+}
+
+Person::~Person() {
+    cout << "Person::~Person():"; println();
+}
+
+void Person::printMembers(ostream* pout)   {
+	*pout << name <<" " <<  id << " " << weight << " " << (married? true : false) <<" :" << address << ":";
+}
+void Person::set(const char *pname, int pid, double pweight,
+    bool pmarried, const char *paddress) {
+	setName(pname);
+    id = pid;
+    weight = pweight;
+    married = pmarried;
+    setAddress(paddress);
+}
+
+void Person::inputMembers(istream* pin)   {
+	if (!*pin) return;
+	*pin >> name;
+	*pin >> id;
+	*pin >> weight;
+	*pin >> married;
+	pin->getline(address, sizeof(address), ':');
+	pin->getline(address, sizeof(address), ':');
+}
+
+void Person::whatAreYouDoing() {
+	cout << name << " is taking a rest." << endl;
+}
+
+bool Person::isSame(const char* pname, int pid) {
+	return (strcmp(name,pname)==0 && id == pid);
+}
+
+/******************************************************************************
+ * User Interface
+ ******************************************************************************/
+// 기본적인 입력과 관련된 전역 함수들을 UI라는 이름공간 내부에 정의함
+
+namespace UI {
+
+bool echo_input = false;
+string line, emptyLine;
+
+// 입력에서 정수 대신 일반 문자가 입력되었는지 체크하고 에러 발생시 에러 메시지 출력
+bool checkInputError(istream* pin, const string msg) {
+    if (!(*pin)) { // 에러가 발생했다면
+        cout << msg;  // 에러 메시지를 출력
+        pin->clear(); // 에러 발생 상태정보를 리셋함; 그래야 다음 문장에서 읽을 수 있음
+        getline(*pin, emptyLine); // 에러가 발생한 행 전체를 읽어 데이터를 버림
+        return true;
+    }
+    return false;
+}
+
+// 정수나 실수를 입력해야 하는 곳에 일반 문자열을 입력한 경우의 에러 체크
+bool checkDataFormatError(istream* pin) {
+    return checkInputError(pin, "Input-data format MISMATCHED\n");
+}
+
+// 한 사람의 정보 즉, 각 멤버 데이터를 순서적으로 입력 받아 p에 저장하고
+// 입력 중 입력 데이터에 오류가 있는지 확인하고 오류가 있을 시 에러 메시지를 출력한다.
+bool inputPerson(Person* p) {
+    cout << "input person information:" << endl;
+    p->input(&cin);
+    if (checkDataFormatError(&cin)) return false;
+    if (echo_input) p->println(); // 자동체크에서 사용됨
+    return true;
+}
+
+// 하나의 정수를 입력 받음; 정수가 아닌 아닌 문자열 입력시 에러 메시지 출력 후 재입력 받음
+int getInt(const string msg) {
+    for (int value; true; ) {
+        cout << msg;
+        cin >> value;
+        if (echo_input) cout << value << endl; // 자동체크 시 출력됨
+        if (checkInputError(&cin, "Input an INTEGER.\n"))
+            continue;
+        getline(cin, emptyLine); // skip [enter] after the number
+        return value;
+    }
+}
+
+// 하나의 양의 정수를 입력 받음; 음수 입력시 에러 메시지 출력 후 재입력 받음
+int getPositiveInt(const string msg) {
+    int value;
+    while ((value = getInt(msg)) < 0)
+        cout << "Input a positive INTEGER." << endl;
+    return value;
+}
+
+// 0~(size-1)사이의 선택된 메뉴 항목 또는 리스트의 항목의 인덱스 값을 리턴함
+// 존재하지 않는 메뉴항목을 선택한 경우 에러 출력
+int getIndex(const string msg, int size) {
+    while (true) {
+        int index = getPositiveInt(msg);
+        if (0 <= index  && index < size) return index;
+        cout << index << ": OUT of selection range(0 ~ "
+             << size-1 << ")" << endl;
+    }
+}
+
+// 사용자에게 메뉴를 보여주고 사용자가 선택한 메뉴항목의 인덱스를 리턴함
+int selectMenu(const string menuStr, int menuItemCount) {
+    cout << endl << menuStr;
+    return getIndex("Menu item number? ", menuItemCount);
+}
+
+} // namespace UI
+
+/******************************************************************************
+ * ch3_2: CurrentUser class
+ ******************************************************************************/
+
+class CurrentUser
+{
+    Person user;
+
+public:
+    CurrentUser(Person u): user(u) { }  // user(u)는 this->user = u 와 동일한 기능
+    void display();
+    void setter();
+    void getter();
+    void set();
+    void whatAreYouDoing();
+    void isSame();
+    void inputPerson();
+    void run();
+};
+
+void CurrentUser::display() { // Menu item 1
+    user.println();
+}
+
+void CurrentUser::getter() { // Menu item 2
+    cout << "name:" << user.getName() << ", id:" << user.getId() << ", weight:" <<
+            user.getWeight() << ", married:" << user.getMarried() <<
+            ", address:" << user.getAddress() << endl;
+}
+
+void CurrentUser::setter() { // Menu item 3
+    Person ps("ps");
+    ps.setName(ps.getName());
+    ps.setId(user.getId());
+    ps.setWeight(user.getWeight());
+    ps.setMarried(user.getMarried());
+    ps.setAddress(user.getAddress());
+    cout << "ps.setMembers():"; ps.println();
+}
+
+void CurrentUser::set() { // Menu item 4
+    Person ps("ps");
+    ps.set(ps.getName(), user.getId(), user.getWeight(),
+              !user.getMarried(), user.getAddress());
+    cout << "ps.set():"; ps.println();
+}
+
+void CurrentUser::whatAreYouDoing() {  // Menu item 5
+    user.whatAreYouDoing();
+}
+
+void CurrentUser::isSame() { // Menu item 6
+    user.println();
+    Person ps("user"); ps.setId(1);
+    cout << "user.isSame(): "
+         << user.isSame(ps.getName(), ps.getId()) << endl;
+}
+
+void CurrentUser::inputPerson() { // Menu item 7
+    if (UI::inputPerson(&user)) // GilDong 1 70.5 true :Jongno-gu, Seoul:
+        display();              // user 1 71.1 true :Gwangju Nam-ro 21:
+}
+
+void CurrentUser::run() {
+    using func_t = void (CurrentUser::*)();
+    func_t func_arr[] = {
+        nullptr, &CurrentUser::display, &CurrentUser::getter, &CurrentUser::setter,
+        &CurrentUser::set, &CurrentUser::whatAreYouDoing,
+        &CurrentUser::isSame, &CurrentUser::inputPerson,
+    };
+    int menuCount = sizeof(func_arr) / sizeof(func_arr[0]); // func_arr[] 배열의 길이
+    string menuStr =
+        "+++++++++++++++++++++ Current User Menu ++++++++++++++++++++++++\n"
+        "+ 0.Logout 1.Display 2.Getter 3.Setter 4.Set 5.WhatAreYouDoing +\n"
+        "+ 6.IsSame 7.InputPerson                                       +\n"
+        "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+
+    while (true) {
+        int menuItem = UI::selectMenu(menuStr, menuCount);
+        if (menuItem == 0) return;
+        (this->*func_arr[menuItem])();
+    }
+}
+// ch3_2: CurrentUser class
+
+/******************************************************************************
+ * ch3_2: MultiManager class
+ ******************************************************************************/
+
+class MultiManager
+{
+    Person person {"p0", 0, 70.0, false, "Gwangju Nam-gu Bongseon-dong 21"};
+
+public:
+    void currentUser() {
+        CurrentUser(person).run();
+    }
+}; // ch3_2: MultiManager class
+
+/******************************************************************************
+ * ch3_1: Class And Object
+ ******************************************************************************/
+
+//Person gps("gps", 1000, 100, true, "gps adress");
+
+class ClassAndObject
+{
+	void defualConstructor() { // Menu item 1
+	        Person ps;    // Person::Person() 기본 생성자 및 소멸자 실행
+	        }
+
+	void constructor() { // Menu item 2: 지역객체의 다양한 생성자 및 소멸자 실행
+	        Person ps1;
+	        Person ps2("ps2");
+	        Person ps3("ps3", 3, 70.3, true, "ps3 address");
+	    }
+
+	void construcor_destructor_inner_inner() {
+	        cout << "\n--- construcor_destructor_inner_inner() begins ---" << endl;
+	        Person ps3("ps3");
+	        cout << "\n- if begins -" << endl;
+	        if (true) {
+	            Person ps4("ps4");
+	        }
+	        cout << "- if ends -\n" << endl;
+	        cout << "- temporary object begins -" << endl;
+	        Person("temp_ps_1"); // 임시객체(이름 없는 객체) 성성 및 소멸
+	        cout << endl;
+	        Person("temp_ps_2").setName("TEMP_PS_2"); // 임시객체 생성,
+	                                                  // setName() 호출, 임시객체 소멸
+	        cout << "- temporary object ends -\n" << endl;
+	        Person ps5("ps5");
+	    }
+
+	    void construcor_destructor_inner() {
+	        cout << "\n--- construcor_destructor_inner() begins ---" << endl;
+	        Person ps2("ps2");
+	        construcor_destructor_inner_inner();
+	        cout << "--- construcor_destructor_inner_inner() returned ---\n" << endl;
+	        Person ps6("ps6");
+	    }
+
+	    void construcorDestructor() { // Menu item 3
+	        Person ps1("ps1");
+	        construcor_destructor_inner();
+	        cout << "--- construcor_destructor_inner() returned ---\n" << endl;
+	    }
+
+	    void global_static_local_objects_inner() {
+	            cout << "\n--- global_static_local_objects_inner() begins ---" << endl;
+	            //static Person sps("sps", 2000, 200, false, "sps adress");
+	    }
+
+	    void globalStaticLocalObjects() { // Menu item 4
+	            global_static_local_objects_inner();
+	            cout << "--- global_static_local_objects_inner() returned ---" << endl;
+	            global_static_local_objects_inner();
+	            cout << "--- global_static_local_objects_inner() returned ---" << endl;
+	    }
+
+public:
+    // 사용자가 선택한 메뉴 항목을 실행하는 멤버 함수(func_arr[menuItem]에 등록된 함수)를 호출
+    void run() {
+        // ClassAndObject의 멤버 함수에 대한 포인터 타입인 새로운 데이타 타입 func_t를 정의함
+        using func_t = void (ClassAndObject::*)();
+        // 위 using 문은 ClassAndObject 클래스의 멤버 함수(리턴 타입이 void 이면서 매개변수가 없는)에
+        // 대한 포인터 타입을 앞으로는 간단히 func_t로 사용하겠다는 의미이다.
+
+        // ClassAndObject의 멤버 함수 포인터들의 배열
+        // 나중에 호출하기 위해 멤버 함수의 이름을 미리 배열에 저장(등록)
+        //     함수 이름이 곧 함수에 대한 포인터임: 즉, 함수의 첫번째 명령어의 주소를 저장함
+        // 등록된 이 멤버 함수들은 나중에 run()에서 호출됨
+        //     (즉, 함수 주소로 jump 해 가서 함수를 실행함)
+
+        func_t func_arr[] = { // 메뉴항목을 실행하는 멤버 함수를 배열에 미리 저장(등록)해 둠
+            nullptr, &ClassAndObject::defualConstructor, &ClassAndObject::constructor,
+            &ClassAndObject::construcorDestructor, &ClassAndObject::globalStaticLocalObjects,
+        };
+        int menuCount = sizeof(func_arr) / sizeof(func_arr[0]);
+        // func_arr[]의 원소의 개수 = 배열 전체 크기 / 한 배열 원소의 크기
+
+        // 화면에 보여 줄 메뉴
+        string menuStr =
+            "+++++++++++ Person Class And Object Menu ++++++++++++\n"
+            "+ 0.Exit 1.DefualConstructor 2.Constructor          +\n"
+            "+ 3.ConstrucorDestructor 4.GlobalStaticLocalObjects +\n"
+            "+++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+
+        while (true) {
+            int menuItem = UI::selectMenu(menuStr, menuCount); // 메뉴 번호 입력 받음
+            if (menuItem == 0) return;
+            (this->*func_arr[menuItem])(); // 선택된 메뉴 항목을 실행할 멤버 함수를 호출함
+            /* 위 문장은 다음을 간단히 한 것이다. 앞으로 계속 나올테니 잘 이해하기 바란다.
+               func_t f = func_arr[menuItem];// 배열에 저장된 멤버 함수 포인터
+               (this->*f)();                 // 멤버함수 포인터를 이용하여 해당 함수를 호출함
+            */
+        }
+    }
+}; // ch3_1: class ClassAndObject
+
+
+/******************************************************************************
+ * Main Menu
+ ******************************************************************************/
+
+/******************************************************************************
+ * 코드 추가 4
+ ******************************************************************************/
+// 기존의 MainMenu 클래스를 아래 코드로 교체하라.
+//----------------------------------------------------------------------------
+
+class MainMenu
+{
+public:
+    void run() {
+        int menuCount = 3; // 상수 정의
+        string menuStr =
+"******************************* Main Menu *********************************\n"
+"* 0.Exit 1.CurrentUser(ch3_2)                                             *\n"
+"* 2.Class:Object(ch3_1)                                                   *\n"
+"***************************************************************************\n";
+
+        while (true) {
+            int menuItem = UI::selectMenu(menuStr, menuCount);
+            if (menuItem == 0) break;
+
+            switch(menuItem) {
+            case 1: MultiManager().currentUser();     break;
+            case 2: ClassAndObject().run();           break;
+            }
+        }
+        cout << "Good bye!!" << endl;
+    }
+}; // class MainMenu
+
+
+/******************************************************************************
+ * run() 함수: 메인 메뉴를 시작함
+ ******************************************************************************/
+void run() {
+    MainMenu().run();
+    // MainMenu 타입의 이름 없는 임시객체를 생성한 후
+    // 그 객체의 run() 멤버함수를 호출함; run()에서 리턴한 후에는 임시객체가 자동 소멸됨
+    // 즉, 위 문장은 아래 두 문장과 동일한 기능임
+    // MainMenu mm;
+    // mm.run();
+}
+
+/******************************************************************************
+ * Program Test: Automatic Error Check
+ ******************************************************************************/
+#if AUTOMATIC_ERROR_CHECK
+#include "check_error.h"
+#endif
+
+/******************************************************************************
+ * main() 함수
+ ******************************************************************************/
+int main() {
+    cout << boolalpha;  // 11장에서 배움; bool 타입 값을 0, 1 대신 true, false로 출력하도록 설정
+    cin >> boolalpha;   // bool 타입 값을 0, 1 대신 true, false로 입력 받도록 설정
+
+#if AUTOMATIC_ERROR_CHECK
+    evaluate(true);   // 각 문제에 대해 단순히 O, X만 확인하고자 할 때는 false
+#else
+    run();
+#endif
+}
+
+//-----------------------------------------------------------------------------
+// ch2.cpp 소스 끝
+//-----------------------------------------------------------------------------
